fb6e7bdea689bbce19017f949531f267d323b0e5
David Park <dpark@junipersquare.com>
2019-07-26 20:11:06 -0700

    adds more notes

--- hello.py
+++ hello.py
@@ -16,4 +16,22 @@ print(favorite_language.rstrip())
 print(favorite_language.strip())
 print(favorite_language.lstrip())
 
-# continue on the zen of python
+# Ch 3 Lists
+bicycles = ['trek', 'cannondale', 'redline']
+print(bicycles[-2]) # returns second to last element
+print(f'my first bicycle was a {bicycles[-1]}')
+bicycles.append('schwinn') # adds an entry
+del bicycles[1] # removes cannondale
+# pop can also delete at any index. Use it when you 
+# want to return the popped value.
+bicycles.remove('trek') # removes trek
+print(bicycles)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True) # mutates original array
+print(cars)
+print(sorted(cars)) # does not mutate original array
+print(cars) # does not mutate original array
+print(len(cars))
+
+#start from Avoiding Index Errors in ch3
