aadf94ddf3d1a1c9b5f0996e67a49f2e8971f7f6
David Park <dpark@junipersquare.com>
2019-08-05 09:45:17 -0700

    completes functions chapter

---
+++ making_pizzas.py
@@ -0,0 +1,8 @@
+from pizza import make_pizza
+# If you just import make_pizza then must use dot notation
+# Can alias function as such:
+# from pizza import make_pizza as mp
+# or alias an entire module with import pizza as p
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
