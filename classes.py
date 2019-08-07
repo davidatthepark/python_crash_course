751831ef88a6dcf88e348ac0a25d0f877b03e334
David Park <dpark4657@gmail.com>
2019-08-07 09:18:20 -0700

    creates battery class

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 class Dog:
     """A simple attemp to model a dog."""
-    
+
     def __init__(self, name, age):
         """Initialize name and age attributes."""
         self.name = name
@@ -9,7 +9,7 @@ class Dog:
     def sit(self):
         """Simulate a dog sitting in response to a command."""
         print(f"{self.name} is now sitting.")
-    
+
     def roll_over(self):
         """Simulate rolling over in response to a command."""
         print(f"{self.name} rolled over!")
@@ -47,17 +47,23 @@ class Car:
     def increment_odometer(self, miles):
         self.odometer_reading += miles
 
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size=75):
+        """Initialize the battery's attributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f'This car has a {self.battery_size}-kWh battery.')
+
 class ElectricCar(Car):
     """Represents aspects of a car, specific to electric vehicles."""
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class"""
         super().__init__(make, model, year)
-        self.battery_size = 75
-    
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
+        self.battery = Battery()
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
